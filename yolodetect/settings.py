0a8c311a1d94423dcd5db5cf31ea1393400e1865
Jijiluzana225 <jiji.luzana@gmail.com>
2024-12-18 15:33:18 +0800

    Update settings.py
    ss

--- yolodetect/settings.py
+++ yolodetect/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'yolodetect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Viza0225!',
+        'HOST': 'localhost',  # Use 'localhost' if on the same machine or an IP address if remote
+        'PORT': '5432',  # Default PostgreSQL port, change if needed
     }
 }
 
