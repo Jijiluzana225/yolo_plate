c4d8b04369ddf54e8b2bfeac438b801eb9c49d67
Jijiluzana225 <jiji.luzana@gmail.com>
2024-12-20 08:12:34 +0800

    d
    d

--- detection/urls.py
+++ detection/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('video_feed/', views.video_feed, name='video_feed'),
+    path('', views.landing, name='landing'),
+    path('contact/', views.contact, name='contact'),    # Contact form URL
 ]
